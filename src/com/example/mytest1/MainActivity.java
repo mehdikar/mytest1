da3227d6b71956af38497a6ef928b76c6bbdf9a1
mehdikar <mehdi.kargar@outlook.com>
2014-01-23 17:51:05 -0500

    adding the webview

--- src/com/example/mytest1/MainActivity.java
+++ src/com/example/mytest1/MainActivity.java
@@ -1,22 +1,35 @@
 package com.example.mytest1;
 
 import android.os.Bundle;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.view.Menu;
+import android.webkit.WebView;
 
 public class MainActivity extends Activity {
 
+	
+	@SuppressLint("JavascriptInterface")
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-	}
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+		WebView browser = (WebView) findViewById(R.id.webView1);
+		browser.getSettings().setJavaScriptEnabled(true);
+		browser.addJavascriptInterface(new JavaScriptInterface(this), 
+				                      "AndroidInterface");
+		
+		// if the html file is in the app's memory space use:
+		   browser.loadUrl("file:///android_asset/my_local_webpage1.html");
+
+		// if the file is in the app's SD card use:
+		// browser.loadUrl("file:///sdcard/my_local_webpage1.html");
+		   
+		// CAUTION: Manifest must include
+		// <uses-permission android:name="android.permission.INTERNET"/>
+		// <uses-permission android:name="android.permission.READ_EXTERNAL_STORAGE"/>
+
+	}//onCreate	
 
 }
